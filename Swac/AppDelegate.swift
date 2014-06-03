f244c41ca4ae68892f6ec6682d94921de8bae3f0
Ullrich Schäfer <ullrich@seidbereit.de>
2014-06-03 16:12:09 +0200

    Initial commit

---
+++ Swac/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  Swac
+//
+//  Created by Ullrich Schäfer on 03/06/14.
+//  Copyright (c) 2014 ullrich. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        // Override point for customization after application launch.
+        self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.makeKeyAndVisible()
+        
+        
+        // just trying to call anything from RAC
+        
+        RACSubject()
+        
+        
+        return true
+    }
+}
+
---
+++ Swac/Bridging.h
@@ -0,0 +1,9 @@
+//
+//  Bridging.h
+//  Swac
+//
+//  Created by Ullrich Schäfer on 03/06/14.
+//  Copyright (c) 2014 ullrich. All rights reserved.
+//
+
+#import <ReactiveCocoa/ReactiveCocoa.h>
